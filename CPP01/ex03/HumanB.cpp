92cb41f1ddc77e48cc91ffaaf2f15e5ff326527e
Rui Pires <rpires-c@c1r2s2.42porto.com>
2025-05-20 15:39:58 +0100

    CPP01 ex03 done

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: rpires-c <rpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 14:11:32 by rpires-c          #+#    #+#             */
-/*   Updated: 2025/05/20 15:32:01 by rpires-c         ###   ########.fr       */
+/*   Updated: 2025/05/20 15:39:20 by rpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ HumanB::~HumanB(){}
 
 void	HumanB::attack()
 {
-	if (!this->weapon)
-		std::cout << this->name << " is tries to attacks but has no weapon " << std::endl;
-	else
+	if (this->weapon)
 		std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	else
+		std::cout << this->name << " is tries to attacks but has no weapon " << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon &weapon)
