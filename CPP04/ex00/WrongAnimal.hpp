36fded592f69f099460ffc706eacff414cc64e03
Rui Pires <rpires-c@c2r5s10.42porto.com>
2025-07-29 18:32:15 +0100

    CPP04 ex00 forgot to add WrongAnimal and WrongCat

---
+++ CPP04/ex00/WrongAnimal.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpires-c <rpires-c@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 18:25:17 by rpires-c          #+#    #+#             */
+/*   Updated: 2025/07/29 18:28:30 by rpires-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGANIMAL_HPP
+# define WRONGANIMAL_HPP
+
+#include <iostream>
+
+class WrongAnimal
+{
+	protected:
+		std::string type;
+
+	public:
+		WrongAnimal();
+		WrongAnimal(std::string type);
+		WrongAnimal(const WrongAnimal &src);
+		WrongAnimal &operator=(const WrongAnimal &rhs);
+
+		std::string	getType() const;
+		virtual void	makeSound() const;
+};
+
+#endif
---
+++ CPP04/ex00/WrongCat.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongWrongCat.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpires-c <rpires-c@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 18:24:29 by rpires-c          #+#    #+#             */
+/*   Updated: 2025/07/29 18:30:40 by rpires-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGCAT_HPP
+# define WRONGCAT_HPP
+
+# include "Animal.hpp"
+
+class WrongCat : public Animal
+{
+	public:
+		WrongCat();
+		WrongCat(const WrongCat &other);
+		WrongCat &operator=(const WrongCat &other);
+		~WrongCat();
+
+		void	makeSound() const;
+};
+
+#endif
