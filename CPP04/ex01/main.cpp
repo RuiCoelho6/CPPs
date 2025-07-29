4ab9c226feafe0a83bdd6b878787433fb272d8ce
Rui Pires <rpires-c@c2r5s10.42porto.com>
2025-07-29 16:43:45 +0100

    CPP04 small update for organization

---
+++ CPP04/ex01/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpires-c <rpires-c@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 15:06:38 by rpires-c          #+#    #+#             */
+/*   Updated: 2025/07/29 15:59:47 by rpires-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int main()
+{
+const Animal* meta = new Animal();
+const Animal* j = new Dog();
+const Animal* i = new Cat();
+std::cout << j->getType() << " " << std::endl;
+std::cout << i->getType() << " " << std::endl;
+i->makeSound();
+j->makeSound();
+meta->makeSound();
+return 0;
+}
